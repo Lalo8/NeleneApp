9b87e242a7ffb260985670f6999a509c03c004b0
Elo <wanang.elodie@hotmail.fr>
2017-11-07 19:38:09 +0100

    package.json OK

--- myvueBulma/src/api/organisations.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-const service = axios.create({
-  baseURL: 'http://localhost:3000/api/organisations',
-});
-
-export function getOrganisations() {
-    return service.get('/').then(res => res.data)
-}
